dfabd836bfd60823a1c5592ff2a664976aaa571d
tiotdev <tiotdev@tiot.de>
2020-02-25 19:13:43 +0530

    Add solo travel community

--- hive/utils/post.py
+++ hive/utils/post.py
@@ -48,7 +48,7 @@ def post_basic(post):
     category = post['category']
 
     # TravelFeed communities
-    hive_communities = ['hive-184437', 'hive-177777', 'hive-188888', 'hive-199999', 'hive-155555', 'hive-122222', 'hive-147474', 'hive-100001', 'hive-166666', 'hive-144444', 'hive-133337', 'hive-104387']
+    hive_communities = ['hive-184437', 'hive-177777', 'hive-188888', 'hive-199999', 'hive-155555', 'hive-122222', 'hive-147474', 'hive-100001', 'hive-166666', 'hive-144444', 'hive-133337', 'hive-104387', 'hive-100705']
 
     # Mark valid TravelFeed posts
     is_travelfeed = ('travelfeed' in tags or category in hive_communities) and ('introduceyourself' in tags or len(body.split(" ")) > 240)
